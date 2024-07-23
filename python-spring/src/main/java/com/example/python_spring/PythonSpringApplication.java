702b66f26c10c3628fe92a5d0c004ac015ebfd4c
rumos <gyunjin3@gmail.com>
2024-07-23 23:12:52 +0900

    Initial Spring boot Project

---
+++ python-spring/src/main/java/com/example/python_spring/PythonSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.python_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PythonSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PythonSpringApplication.class, args);
+	}
+
+}
---
+++ python-spring/src/test/java/com/example/python_spring/PythonSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.python_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PythonSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
